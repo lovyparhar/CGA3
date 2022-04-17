5bd380d923a1ea846aa4e37324143bad37330050
Lovejeet Singh <lovejeetsingh@Lovejeets-MacBook-Pro.local>
2022-04-17 20:39:44 +0530

    Models added and builtin material + lighting used

--- Lovejeet/app.js
+++ Lovejeet/app.js
@@ -60,41 +60,83 @@ import * as THREE from 'three';
 import { PLYLoader } from 'https://unpkg.com/three@0.139.2/examples/jsm/loaders/PLYLoader.js';
 
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.shadowMap.enabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 15 );
-camera.position.set( 2, 2, 2);
+camera.position.set( 2, 1, 2);
 camera.lookAt( 0, 0, 0 );
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xf1f1f1 );
+scene.background = new THREE.Color( 0xa1a1a1 );
 
+// Adding light to the scene
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(2, 2, 2);
+light.castShadow = true;
+scene.add(light);
+
+
+
+
+
+
+// ---------- Adding objects to the scene ------------
 const loader = new PLYLoader();
-loader.load( './teapot.ply', function ( geometry ) {
 
-    geometry.computeVertexNormals();
+let teapotMesh;
+let isLoaded = new Promise(function(resolve, reject) {
+    loader.load( './teapot.ply', function ( geometry ) {
+        geometry.computeVertexNormals();
 
-    const material = new THREE.MeshStandardMaterial( { color: 0x0055ff, flatShading: true } );
-    const mesh = new THREE.Mesh( geometry, material );
+        const material = new THREE.MeshPhongMaterial( { color: 0x0055ff, flatShading: false } );
+        const mesh = new THREE.Mesh( geometry, material );
 
-    mesh.position.y = - 0.2;
-    mesh.position.z = 0.3;
-    mesh.rotation.x = - Math.PI / 2;
-    mesh.scale.multiplyScalar(0.2);
+        mesh.position.y = 0;
+        mesh.position.z = 0;
+        mesh.rotation.x = - Math.PI / 2;
+        mesh.scale.multiplyScalar(0.2);
 
-    mesh.castShadow = true;
-    mesh.receiveShadow = true;
+        mesh.castShadow = true;
+        teapotMesh = mesh;
+        resolve("Teapot mesh set");
+    });
+});
 
-    console.log(mesh);
-    console.log(mesh.scale)
-    scene.add( mesh );
-} );
+// After this point in code, the code can safely assume that the teapotMesh variable is set
+let res = await isLoaded;
+scene.add(teapotMesh);
 
-console.log(scene);
+// Adding the plane beneath the objects
+let planeGeometry = new THREE.PlaneGeometry(40,40);
+let planeMaterial = new THREE.MeshStandardMaterial( { color: 0x325453, flatShading: true } );
+planeMaterial.side = THREE.DoubleSide
+let planeMesh = new THREE.Mesh( planeGeometry, planeMaterial );
+planeMesh.rotation.x += Math.PI/2;
+planeMesh.receiveShadow = true;
+scene.add(planeMesh);
+
+// Adding a shpere
+let sphereGeometry = new THREE.SphereGeometry(0.2,40, 40);
+let sphereMaterial = new THREE.MeshPhongMaterial( { color: 0x33eeff, flatShading: true } );
+sphereMaterial.side = THREE.DoubleSide
+let sphereMesh = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphereMesh.position.y += 0.2;
+sphereMesh.position.x += 1;
+sphereMesh.position.z += 0.5;
+sphereMesh.castShadow = true;
+scene.add(sphereMesh);
+
+// -------------------------------------------------------
 
 function animate() {
+    // teapotMesh.rotation.x += 0.01;
+    // teapotMesh.rotation.y += 0.01;
+    // teapotMesh.rotation.z += 0.01;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
