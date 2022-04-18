ffe3111360a62991be4d4ff7b90826dc07e08085
Lovejeet Singh <lovejeetsingh@Lovejeets-MacBook-Pro.local>
2022-04-18 15:18:10 +0530

    Gouraud shader added

--- Lovejeet/app.js
+++ Lovejeet/app.js
@@ -127,8 +127,6 @@ let sphereConfig = {
     alpha : 100
 }
 
-// let sphereMaterial = new THREE.MeshPhongMaterial( { color: 0x33eeff, flatShading: true } );
-
 let sphereMaterial = new THREE.ShaderMaterial( {
     uniforms: {
         u_lightPos: {value: sphereConfig.lightPos},
@@ -145,7 +143,10 @@ let sphereMaterial = new THREE.ShaderMaterial( {
 	vertexShader: phongVertexShader,
 	fragmentShader: phongFragmentShader
 } );
+
 sphereMaterial.side = THREE.DoubleSide
+
+
 
 
 let sphereMesh = new THREE.Mesh( sphereGeometry, sphereMaterial );
@@ -157,17 +158,118 @@ scene.add(sphereMesh);
 
 
 
+let models = [teapotMesh, sphereMesh];
+let phongConfig = [teapotConfig, sphereConfig];
+let phongMaterial = [teapotMaterial, sphereMaterial];
+
+teapotConfig = {
+    lightPos : new THREE.Vector3(2, 2, 2),
+    cameraPos : new THREE.Vector3(
+        camera.position.x,
+        camera.position.y,
+        camera.position.z,
+    ),
+
+    diffuseColor : new THREE.Vector4(0.3,0.4,0.9,1.0),
+    kDiffuse : 1.0,
+
+    ambientColor : new THREE.Vector4(0.3,0.4,0.9,1.0),
+    kAmbient : 0.1,
+
+    specularColor : new THREE.Vector4(1.0,1.0,1.0,1.0),
+    kSpecular : 0.7,
+    alpha : 100
+}
+
+teapotMaterial = new THREE.ShaderMaterial( {
+    uniforms: {
+        u_lightPos: {value: teapotConfig.lightPos},
+        u_lightTarget: {value: teapotConfig.lightTarget},
+        u_cameraPos: {value: teapotConfig.cameraPos},
+        u_diffuseColor: {value: teapotConfig.diffuseColor},
+        u_kDiffuse: {value: teapotConfig.kDiffuse},
+        u_ambientColor: {value: teapotConfig.ambientColor},
+        u_kAmbient: {value: teapotConfig.kAmbient},
+        u_specularColor: {value: teapotConfig.specularColor},
+        u_kSpecular: {value: teapotConfig.kSpecular},
+        u_alpha: {value: teapotConfig.alpha}
+    },
+	vertexShader: gouraudVertexShader,
+	fragmentShader: gouraudFragmentShader
+} );
+
+teapotMaterial.side = THREE.DoubleSide;
+
+sphereConfig = {
+    lightPos : new THREE.Vector3(2, 2, 2),
+    cameraPos : new THREE.Vector3(
+        camera.position.x,
+        camera.position.y,
+        camera.position.z,
+    ),
+
+    diffuseColor : new THREE.Vector4(0.8,0.3,0.3,1.0),
+    kDiffuse : 1.0,
+
+    ambientColor : new THREE.Vector4(0.8,0.3,0.3,1.0),
+    kAmbient : 0.1,
+
+    specularColor : new THREE.Vector4(1.0,1.0,1.0,1.0),
+    kSpecular : 0.7,
+    alpha : 100
+}
+
+sphereMaterial = new THREE.ShaderMaterial( {
+    uniforms: {
+        u_lightPos: {value: sphereConfig.lightPos},
+        u_lightTarget: {value: sphereConfig.lightTarget},
+        u_cameraPos: {value: sphereConfig.cameraPos},
+        u_diffuseColor: {value: sphereConfig.diffuseColor},
+        u_kDiffuse: {value: sphereConfig.kDiffuse},
+        u_ambientColor: {value: sphereConfig.ambientColor},
+        u_kAmbient: {value: sphereConfig.kAmbient},
+        u_specularColor: {value: sphereConfig.specularColor},
+        u_kSpecular: {value: sphereConfig.kSpecular},
+        u_alpha: {value: sphereConfig.alpha}
+    },
+	vertexShader: gouraudVertexShader,
+	fragmentShader: gouraudFragmentShader
+} );
+
+sphereMaterial.side = THREE.DoubleSide;
+
+let gouraudConfig = [teapotConfig, sphereConfig];
+let gouraudMaterial = [teapotMaterial, sphereMaterial];
+let currentMaterial = 0 // 0 for phong, 1 for gouraud 
 
 
 
 
+document.addEventListener('keydown', function(event) {
+    if(event.code == 'KeyS') {
+        currentMaterial = (currentMaterial + 1)%2;
+        if(currentMaterial == 0) {
+            models[0].material = phongMaterial[0];
+            models[1].material = phongMaterial[1];
+        }
+        else if(currentMaterial == 1) {
+            models[0].material = gouraudMaterial[0];
+            models[1].material = gouraudMaterial[1];
+        }
+    }
+});
 
 
 
+// // Object selection mechanism
+// const indToObjectName = ["Teapot, Ball"];
+// let selectedMesh = 2;
+// document.addEventListener('keydown', function() {
 
-const indToObjectName = ["Teapot, Ball"];
+// });
 
-// -------------------------------------------------------
+
+// ----------------------------------------------------
 
 function animate() {
     // teapotMesh.rotation.x += 0.01;
---
+++ Lovejeet/gouraudVertexShader.glsl
@@ -0,0 +1,59 @@
+uniform vec3 u_lightPos;
+uniform vec3 u_cameraPos;
+uniform vec4 u_diffuseColor;
+uniform float u_kDiffuse;
+uniform vec4 u_ambientColor;
+uniform float u_kAmbient;
+uniform vec4 u_specularColor;
+uniform float u_kSpecular;
+uniform float u_alpha;
+
+varying vec4 v_color;
+
+
+void main() {
+
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
+
+
+
+    vec3 unitNormal = normalize(normalMatrix * normal);
+    vec3 position = (modelMatrix * vec4( position, 1.0)).xyz;
+
+    // The vector from the surface to light in world space
+    vec3 revLightWorld = normalize(u_lightPos - position);
+
+    // The vector from the surface to light in view space
+    vec3 revLightView = (viewMatrix * vec4( revLightWorld, 0.0)).xyz;
+
+
+
+    // Taking care of diffused light
+    float diffuseAmt = max(0.0, dot(revLightView, unitNormal));
+    vec3 diffuseCont = vec3(u_kDiffuse * u_diffuseColor.xyz * diffuseAmt);
+
+
+
+    // Taking care of specular lighting
+    vec3 vertToCam = normalize(u_cameraPos - position);
+    vec3 halfwayVectorWorld = normalize(vertToCam + revLightWorld);
+    vec3 halfwayVectorView = (viewMatrix * vec4( halfwayVectorWorld, 0.0)).xyz;
+    
+    float specAmt = pow(
+        max(0.0, dot(halfwayVectorView, unitNormal)),
+        u_alpha
+    );
+
+    vec3 specCont = vec3(u_kSpecular * u_specularColor.xyz * specAmt);
+
+
+
+    // Taking care of ambient lighting
+    vec3 ambientCont = vec3(u_kAmbient * u_ambientColor);
+
+    v_color = vec4 (
+        diffuseCont + ambientCont + specCont, 
+        u_diffuseColor.w
+    );
+}
+
